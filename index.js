4cd8a6f2194bb9dd835294c7d8e51a0bb5d653c0
Dylan <dylan.moulin9272@gmail.com>
2026-04-20 19:32:50 +0200

    update matches value : accept all addresses after https://www.eventtia.com/

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-let selects = document.querySelectorAll('.vsplit > #element');
-let input = document.querySelectorAll(' #input .short_description');
-let title = '';
 console.log("ok ma poule");
-selects.forEach(select => {
-    console.log(select.value + '\n')
-})
+// let select = document.getElementById('event_attendees-aa0914db-0551-46e4-b85b-7a1902ff02d0');
+// let input = document.querySelectorAll(' #input .short_description');
+// let title = '';
+// console.log(select.value + '\n')
