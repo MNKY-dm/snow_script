544bda33a065edb5c676bd5165af67fc619bc7c9
Dylan MOULIN <d.moulin@allianceautomotive.fr>
2026-04-20 16:02:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let selects = document.querySelectorAll('.vsplit > #element');
+let input = document.querySelectorAll(' #input .short_description');
+let title = '';
+console.log("ok ma poule");
+selects.forEach(select => {
+    console.log(select.value + '\n')
+})
