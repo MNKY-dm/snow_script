e9591c86844487d153531b2f323533f9102bb557
Dylan <dylan.moulin9272@gmail.com>
2026-04-22 15:07:00 +0200

    tests ok - deal with forms datas ok

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 console.log("ok ma poule");
-// let select = document.getElementById('event_attendees-aa0914db-0551-46e4-b85b-7a1902ff02d0');
-// let input = document.querySelectorAll(' #input .short_description');
-// let title = '';
-// console.log(select.value + '\n')
+let inputFirstName = document.getElementById('firstname-aa0914db-0551-46e4-b85b-7a1902ff02d0');
+let selectCountry = document.getElementById('phone_ext-aa0914db-0551-46e4-b85b-7a1902ff02d0');
+let title = '';
+
+let firstName = '';
+let country = ''
+inputFirstName.addEventListener('input', () => {
+    firstName = inputFirstName.value;
+    console.log(firstName)
+})
+
+selectCountry.addEventListener('change', () => {
+    country = selectCountry.value;
+    console.log(country)
+    inputFirstName.value += country;
+})
