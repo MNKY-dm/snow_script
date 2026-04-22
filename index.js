e52b1fbbc2e83ac1ea8bc9c669e2a919ed6a9049
Dylan MOULIN <d.moulin@allianceautomotive.fr>
2026-04-22 15:58:44 +0200

    v0.0.2 : injections in snow work. logic must be improved

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
-console.log("ok ma poule");
-let inputFirstName = document.getElementById('firstname-aa0914db-0551-46e4-b85b-7a1902ff02d0');
-let selectCountry = document.getElementById('phone_ext-aa0914db-0551-46e4-b85b-7a1902ff02d0');
-let title = '';
+console.log("Extension [snow_script] chargée : OK");
 
-let firstName = '';
-let country = ''
-inputFirstName.addEventListener('input', () => {
-    firstName = inputFirstName.value;
-    console.log(firstName)
-})
+let selectCategory = document.getElementById('incident.category');
+let selectSubCategory = document.getElementById('incident.subcategory');
+let selectItem = document.getElementById('incident.u_item');
+let inputCI = document.getElementById('incident.cmdb_ci');
 
-selectCountry.addEventListener('change', () => {
-    country = selectCountry.value;
-    console.log(country)
-    inputFirstName.value += country;
-})
+let inputShortDescription = document.getElementById('incident.short_description');
+
+
+
+function getShortDescription() {
+    let shortDescription = ''
+    inputShortDescription.addEventListener('input', () => {
+        shortDescription = inputShortDescription.value;
+        console.log("shortdescription : " + shortDescription)
+    })
+    return shortDescription;
+}
+
+function getCategory() {
+    let category = '';
+    selectCategory.addEventListener('change', () => {
+        category = selectCategory.value;
+        console.log("category : " + category)
+    })
+    return category
+}
+
+function getSubCategory() {
+    let subCategory = ''
+    selectSubCategory.addEventListener('change', () => {
+        subCategory = selectSubCategory.value;
+        console.log("subcategory : " + subCategory)
+    })
+    return subCategory;
+}
+
+function getItem() {
+    let item = '';
+    selectItem.addEventListener('change', () => {
+        item = selectItem.value;
+        console.log("item : " + item)
+    })
+    return item;
+}
+
+function getCI() {
+    let CI = ''
+    inputCI.addEventListener('input', () => {
+        CI = selectItem.value;
+        console.log("CI : " + CI)
+    })
+    return CI;
+}
+
+function setShortDescription() {
+    let shortDescription = getShortDescription();
+    let category = getCategory().toUpperCase();
+    let subCategory = getSubCategory().toUpperCase();
+    let item = getItem().toUpperCase();
+    let CI = getCI().toUpperCase();
+
+    let finalShortDescription = category + ' - ' + subCategory + ' - ' + item + ' - ' + CI + ' - ' + shortDescription;
+    inputShortDescription.value = finalShortDescription;
+}
+
+setShortDescription()
+
