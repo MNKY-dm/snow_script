c52c2dbd5524bc36ee8146d9196b47e11892b69e
Dylan MOULIN <d.moulin@allianceautomotive.fr>
2026-04-23 08:27:29 +0200

    v1.0.0 : it now works perfectly sometimes. Need to know why it doesn't work in other cases.

--- index.js
+++ index.js
@@ -3,60 +3,65 @@ console.log("Extension [snow_script] chargée : OK");
 let selectCategory = document.getElementById('incident.category');
 let selectSubCategory = document.getElementById('incident.subcategory');
 let selectItem = document.getElementById('incident.u_item');
-let inputCI = document.getElementById('incident.cmdb_ci');
+let inputCI = document.querySelector('#sys_display\\.incident\\.cmdb_ci');
 
 let inputShortDescription = document.getElementById('incident.short_description');
 
 function setShortDescription() {
-    console.log("setShortDescription en cours");
+    // console.log("setShortDescription en cours");
+    let tab = inputShortDescription.value.split('- ');
+    let shortDescription = tab[tab.length - 1];
     let finalShortDescription = category + ' - ' + subCategory + ' - ' + item + ' - ' + CI + ' - ' + shortDescription;
-    console.log("finalshortdescription : " + finalShortDescription);
+    // console.log("finalshortdescription : " + finalShortDescription);
     inputShortDescription.value = finalShortDescription;
 }
 
 let shortDescription = ''
 inputShortDescription.addEventListener('input', () => {
     shortDescription = inputShortDescription.value;
-    console.log("shortdescription : " + shortDescription)
+    // console.log("shortdescription : " + shortDescription)
     setShortDescription()
 })
-console.log("getShortDescription OK");
+// console.log("getShortDescription OK");
 
 let category = '';
 selectCategory.addEventListener('change', () => {
     category = selectCategory.value;
-    console.log("category : " + category)
+    category = category.toUpperCase();
+    // console.log("category : " + category)
     setShortDescription()
 })
-category = category.toUpperCase()
-console.log("getCategory OK");
+// console.log("getCategory OK");
 
 let subCategory = ''
 selectSubCategory.addEventListener('change', () => {
     subCategory = selectSubCategory.value;
-    console.log("subcategory : " + subCategory)
+    subCategory = subCategory.toUpperCase();
+    // console.log("subcategory : " + subCategory)
     setShortDescription()
 })
-subCategory = subCategory.toUpperCase();
-console.log("getSubCategory OK");
+// console.log("getSubCategory OK");
 
 let item = '';
 selectItem.addEventListener('change', () => {
     item = selectItem.value;
-    console.log("item : " + item)
+    item = item.toUpperCase();
+    // console.log("item : " + item)
     setShortDescription()
 })
-item = item.toUpperCase();
-console.log("getItem OK");
+// console.log("getItem OK");
 
 let CI = ''
 inputCI.addEventListener('input', () => {
-    CI = selectItem.value;
-    console.log("CI : " + CI)
-    setShortDescription()
+    setTimeout(() => {
+        CI = inputCI.value;
+        CI = CI.toUpperCase();
+        setShortDescription()
+        console.log("CI : " + CI)
+    }, 1000)
+    // setShortDescription()
 })
-CI = CI.toUpperCase();
-console.log("getCI OK");
+// console.log("getCI OK");
 
 
 
